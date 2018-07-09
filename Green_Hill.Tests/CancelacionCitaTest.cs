7dce15c4d3b93456d8d516052a3f6ec9edf4c469
Santiago Suarez Quiroz <suarezquiroz@gmail.com>
2018-07-09 04:03:21 -0500

    Add unit test project

---
+++ Green_Hill.Tests/CancelacionCitaTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Green_Hill.Tests
+{
+    public class CancelacionCitaTest
+    {
+        [Fact]
+        public void CancelarCita()
+        {
+            Assert.True(true, "Cita Cancelada");
+        }
+
+        [Fact]
+        public void CancelarCitaMismoDia()
+        {
+            Assert.True(true, "Cita Cancelada");
+        }
+
+        [Fact]
+        public void CancelarCita24HorasAntes()
+        {
+            Assert.True(true, "Cita Cancelada");
+        }
+    }
+}
---
+++ Green_Hill.Tests/CreacionCitaTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Green_Hill.Tests
+{
+    public class CreacionCitaTest
+    {
+        [Fact]
+        public void CrearCita()
+        {
+            Assert.True(true, "Cita Creada");
+        }
+
+        [Fact]
+        public void CrearCitaMismoDiaMismoPaciente()
+        {
+            Assert.True(true, "Cita Creada");
+        }
+
+        [Fact]
+        public void CrearCitaMismoDiaDiferentePaciente()
+        {
+            Assert.True(true, "Cita Creada");
+        }
+    }
+}
